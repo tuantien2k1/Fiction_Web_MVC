3666d51d6c60e78e10d585636cd85e616d55ea0b
Tien <Tien@DESKTOP-7DJ2RUD>
2023-05-31 00:16:03 +0700

    Cart UI + CRUD

--- Fiction.Models/ShoppingCart.cs
+++ Fiction.Models/ShoppingCart.cs
@@ -23,5 +23,7 @@ namespace Fiction.Models
         [ForeignKey("ApplicationUserId")]
         [ValidateNever]
         public ApplicationUser ApplicationUser { get; set; }
+        [NotMapped]
+        public double Price { get; set; }
     }
 }
