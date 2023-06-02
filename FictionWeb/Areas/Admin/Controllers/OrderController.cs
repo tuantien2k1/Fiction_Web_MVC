74616f472958721409e05ee54bc60cc5ff8f8568
Tien <Tien@DESKTOP-7DJ2RUD>
2023-06-03 02:18:09 +0700

    Only Admin and Employee Can See all Orders

--- FictionWeb/Areas/Admin/Controllers/OrderController.cs
+++ FictionWeb/Areas/Admin/Controllers/OrderController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace FictionWeb.Areas.Admin.Controllers
 {
@@ -64,7 +65,20 @@ namespace FictionWeb.Areas.Admin.Controllers
         [HttpGet]
         public IActionResult GetAll(string status)
         {
-            IEnumerable<OrderHeader> objOrderHeader = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            IEnumerable<OrderHeader> objOrderHeader;
+
+            if(User.IsInRole(SD.Role_Admin)||User.IsInRole(SD.Role_Employee))
+            {
+                objOrderHeader = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            }
+            else
+            {
+                var claimsIdentity = (ClaimsIdentity)User.Identity;
+                var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+                objOrderHeader = _unitOfWork.OrderHeader.GetAll(u => u.ApplicationUserId == userId, includeProperties:"ApplicationUser");
+            }
+
 
             switch (status)
             {
