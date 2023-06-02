0eacdda8abfc1b3c5dabb9154c19e276cdbef68d
Tien <Tien@DESKTOP-7DJ2RUD>
2023-06-02 23:57:54 +0700

    Details UI

--- FictionWeb/wwwroot/js/order.js
+++ FictionWeb/wwwroot/js/order.js
@@ -40,7 +40,7 @@ function loadDataTable(status) {
                 data: 'id',
                 "render": function (data) {
                     return `<div class="w-75 btn-group" role="group">
-                                <a href="/admin/product/details?orderId=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i></a>
+                                <a href="/admin/order/details?orderId=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i></a>
                             </div>`
                 },
                 "width": "10%"
