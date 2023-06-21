9cc61458f44d2975e32fd51798eda1c59afc1a34
Tien <Tien@DESKTOP-7DJ2RUD>
2023-06-21 12:12:53 +0700

    Session Bug

--- FictionWeb/Areas/Customer/Controllers/CartController.cs
+++ FictionWeb/Areas/Customer/Controllers/CartController.cs
@@ -175,6 +175,7 @@ namespace FictionWeb.Areas.Customer.Controllers
                     _unitOfWork.OrderHeader.UpdateStatus(id,SD.StatusApproved, SD.PaymentStatusApproved);
                     _unitOfWork.Save();
                 }
+                HttpContext.Session.Clear();
             }
             List<ShoppingCart> shoppingCarts = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == orderHeader.ApplicationUserId).ToList();
             _unitOfWork.ShoppingCart.RemoveRange(shoppingCarts);
